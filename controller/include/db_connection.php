65f92af6b09a00a09ea8195f2b66fe96c2998b1a
Suhejl Asani <suhejl.asani@edu.tbz.ch>
2020-09-15 10:56:31 +0200

    Changed connection to MYSQL PHP Admin

--- controller/include/db_connection.php
+++ controller/include/db_connection.php
@@ -6,8 +6,17 @@
  $dsn ="odbc:MySQLBeispielLogin";
 
 echo "<font color='red'>DIL Data Interface Layer [ODBC] to MYSQL</font><br>";
+//"mysql:host=192.168.99.100:32769;dbname=Personaldata", "root", "123456"
+$dbconn = new PDO("mysql:host=localhost:3306;dbname=persondata", "root", "");
 
-$dbconn = new PDO("mysql:host=192.168.99.100:32769;dbname=Personaldata", "root", "123456");
 //$dbconn = new PDO("odbc:MYSQL_PERSONALDATA", "root", "123456");
 
+// try{
+//    foreach($dbconn->query("select * from personaldata") as $row){
+//    print_r($row);
+// }
+// }catch(PDOException $e){
+//    print "Error!: " . $e->getMessage() . "<br/>";
+//    die();
+// }
 ?>
