458683a88fe7ea12c8a84641d76eefbd5e02e62b
Suhejl Asani <suhejl.asani@edu.tbz.ch>
2020-09-15 11:10:33 +0200

    Added little validation for email input

--- js/index.js
+++ js/index.js
@@ -4,5 +4,17 @@ document.addEventListener('DOMContentLoaded', function(){
             this.className = this.className.replace(/form__input--error ?/, '') // form__input--error class is removed (replaced with nothing) with Regex. " ?" means, that the whitespace at the end is optional.
         });
 
+        if(input.id === 'email'){
+            input.addEventListener('change', function(){
+
+                var regex = /\S+@\S+\.\S+/;
+                let isValid = regex.test(this.value);                
+                console.log(isValid);
+                
+                if(!isValid){
+                    input.classList.add('form__input--error');
+                }
+            })
+        }
     });
 });
