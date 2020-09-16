f4faaa2af005741e4681e70a3240a0482c15e2c2
Suhejl Asani <suhejl.asani@edu.tbz.ch>
2020-09-16 15:37:43 +0200

    Adding business layer to get personaldata

--- controller/personaldata.php
+++ controller/personaldata.php
@@ -2,6 +2,18 @@
 <?php
 include('include/db_connection.php');
 
+// Read
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $sql_select_data = "SELECT Firstname, Lastname, Birthday, Email, AHV, Personalnumber, Companyname, Department, Jobtitle, Jobdescription FROM personaldata JOIN companydata ON ID_Companydata = Companydata_ID";
+
+    $ppStmt = $dbconn->prepare($sql);
+    $ppStmt->execute();
+    $data = $ppStmt->fetch(PDO::FETCH_ASSOC);
+
+    header('Content-Type: application/json');
+    echo json_encode($data);
+}
+
 // Create
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
