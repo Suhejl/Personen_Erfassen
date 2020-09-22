a08ee3f08e765563b4f114a92e933cb18175ea47
Suhejl <suhejl.asani@edu.tbz.ch>
2020-09-22 16:37:45 +0200

    Modified Stored Procedure

--- database/stored_procedures.sql
+++ database/stored_procedures.sql
@@ -1,54 +1,55 @@
 DELIMITER $$
-CREATE PROCEDURE Masterinsertupdatedelete (IN `Personaldata_ID` INT,  
-                                          IN `Firstname` VARCHAR(50),  
-                                          IN `Lastname` VARCHAR(50),  
-                                          IN `Birtday` DATE, 
-                                          IN `Email` VARCHAR(50),
-                                          IN `AHV` VARCHAR(16),
-                                          IN `Personalnumber` VARCHAR(16),
-										  IN `Phonenumber` INT,
-                                          IN `ID_Companydata` INT,
-                                          IN `StatementType` VARCHAR(50))										
+CREATE PROCEDURE `Stored_Procedure_SELECT_INSERT_UPDATE_DELETE` (IN ID INT,  
+                                          IN firstname VARCHAR(50),  
+                                          IN lastname VARCHAR(50),  
+                                          IN birthday DATE, 
+                                          IN email VARCHAR(50),
+                                          IN ahv VARCHAR(50),
+                                          IN personalnumber INT,
+										  IN phonenumber VARCHAR(50),
+                                          IN id_Companydata INT,
+                                          IN Action VARCHAR(50))										
 BEGIN
-		  IF `StatementType` = 'Insert' THEN
+		IF Action = 'INSERT' THEN
 				INSERT INTO personaldata  
 							(Firstname,  
 							 Lastname,  
-							 Birtday,  
+							 Birthday,  
 							 Email,
                              AHV,
                              Personalnumber,
-                             Phonenumber,
+                            Phonenumber,
                              ID_Companydata)
 							 
-				VALUES     (`Firstname`,  
-							`Lastname`,  
-							`Birtday`,  
-							`Email`,
-                            `AHV`,
-                            `Personalnumber`,
-                            `Phonenumber`,
-                            `ID_Companydata`);
+				VALUES     (firstname,  
+							lastname,  
+							birthday,  
+							email,
+                            ahv,
+                            personalnumber,
+                           	phonenumber,
+                            id_Companydata);
 
-      ELSEIF `StatementType` = 'Select' THEN
-            SELECT * FROM personaldata JOIN companydata ON ID_Companydata = Companydata_ID;
+	  ELSEIF Action = 'SELECTBYID' THEN
+            SELECT * FROM personaldata WHERE Personaldata_ID = ID;
+
+      ELSEIF Action = 'SELECT' THEN
+            SELECT p.Personaldata_ID as 'Personaldata_ID', p.Firstname as 'Firstname', p.Lastname as 'Lastname', p.Birthday as 'Birthday', p.Email as 'Email', p.AHV as 'AHV', p.Personalnumber as 'Personalnumber', p.Phonenumber as 'Phonenumber',
+			 p.ID_Companydata as 'ID_Companydata', c.Companyname as 'Companyname', c.Departement as 'Departement', c.Jobtitle as 'Jobtitle', c.Jobdescription as 'Jobdescription' FROM personaldata p JOIN companydata c ON p.ID_Companydata = c.Companydata_ID;
             
-	  ELSEIF `StatementType` = 'SelectById' THEN
-            SELECT * FROM personaldata WHERE Personaldata_ID=`Personaldata_ID`;
-            
-	   ELSEIF `StatementType` = 'Update' THEN
+	   ELSEIF Action = 'UPDATE' THEN
 				UPDATE personaldata  
-				SET    Firstname=`Firstname`,  
-					   Lastname=`Lastname`,  
-					   Birtday=`Birtday`,  
-					   Email=`Email`,
-                       AHV=`AHV`,
-                       Personalnumber=`Personalnumber`,
-                       Phonenumber=`Phonenumber`
-				WHERE  Personaldata_ID=`Personaldata_ID`;
+				SET    Firstname=firstname,  
+					   Lastname=lastname,  
+					   Birthday=birthday,  
+					   Email=email,
+                       AHV=ahv,
+                       Personalnumber=personalnumber,
+                       Phonenumber=phonenumber
+				WHERE  Personaldata_ID=ID;
                 
-		  ELSEIF `StatementType` = 'Delete' THEN
+		  ELSEIF Action = 'DELETE' THEN
 				DELETE FROM personaldata  
-				WHERE Personaldata_ID = `Personaldata_ID`;
+				WHERE Personaldata_ID = ID;
 		END IF;
 END
