da2602eac7c0c8c7de8c5d5ae926b027f0339b8d
Suhejl <suhejl.asani@edu.tbz.ch>
2020-09-22 09:22:23 +0200

    Added PHP file for functions

---
+++ controller/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function isset_properties($datas)
+{
+    for ($i = 0; $i < count($datas); $i++) {
+        echo "<br>";
+        echo $datas[$i];
+        if (trim($datas[$i]) === null || empty(trim($datas[$i]))) {
+            return false;
+        }
+    }
+    return true;
+}
+
+?>
