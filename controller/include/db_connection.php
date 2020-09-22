54bf7b5d3fbd669966d3ff9266800951e9caac41
Suhejl <suhejl.asani@edu.tbz.ch>
2020-09-22 09:22:37 +0200

    Removed an echo

--- controller/include/db_connection.php
+++ controller/include/db_connection.php
@@ -5,7 +5,6 @@
 //connect to database
  $dsn ="odbc:MySQLBeispielLogin";
 
-echo "<font color='red'>DIL Data Interface Layer [ODBC] to MYSQL</font><br>";
 //"mysql:host=192.168.99.100:32769;dbname=Personaldata", "root", "123456"
 $dbconn = new PDO("mysql:host=localhost:3306;dbname=persondata", "root", "");
 
